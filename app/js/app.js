2adab1cd4f68be0bcd5230511042c6dc352c990b
Vitaly Surkov <vitalya_pro@mail.ru>
2020-03-28 08:36:13 +0500

    #2 header, navbar, greeting section

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+$(document).ready(() => {
+	
+})
 
-	// Custom JS
+$(window).scroll(() => {
+	const navbar = $('.navbar');
 
-});
+	$(document).scrollTop() > $('header .contacts').innerHeight() ? navbar.addClass('fixed-top') : navbar.removeClass('fixed-top')
+})
