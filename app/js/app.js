fc63ad8a2583a52339cca781195c808c978234d0
Vitaly Surkov <vitalya_pro@mail.ru>
2020-03-29 10:11:35 +0500

    #7 reviews done + some fix

--- app/js/app.js
+++ app/js/app.js
@@ -5,12 +5,15 @@ $(document).ready(() => {
 $(window).scroll(() => {
 	const navbar = $('.navbar');
 
-	$(document).scrollTop() > $('header .contacts').innerHeight() ? navbar.addClass('fixed-top') : navbar.removeClass('fixed-top')
+	//$(document).scrollTop() > $('header .contacts').innerHeight() ? navbar.addClass('fixed-top') : navbar.removeClass('fixed-top')
 })
 
 $(".reviews-content").slick({
 	centerMode: true,
 	centerPadding: '200px',
+	arrows: true,
 	infinite: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
 	slidesToShow: 1
 });
